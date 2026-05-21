a4449f4eac5ec634d7e16b8e735c5ea50ed4ecbf
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2026-05-21 10:42:15 -0500

    first commit

---
+++ src/main/java/com/example/api_laboratory_inventory/ApiLaboratoryInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_laboratory_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLaboratoryInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLaboratoryInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_laboratory_inventory/ApiLaboratoryInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_laboratory_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLaboratoryInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
